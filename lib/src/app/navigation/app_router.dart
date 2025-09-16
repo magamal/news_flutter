7b5ec790309a73ef8a8c5244a5ac16bba49b735e
Ahmed Fouad <ahmed3.fouad.ext@orange.com>
2025-09-16 12:44:40 +0300

    - enhancements in navigation 2

--- lib/src/app/navigation/app_router.dart
+++ lib/src/app/navigation/app_router.dart
@@ -1,12 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:news_app/src/features/media/media.dart';
+import 'package:news_app/src/features/news/news.dart';
+import 'package:news_app/src/navigation/app_routes.dart';
 
-import '../../features/media/media.dart';
-import '../../features/news/news.dart';
-import '../../navigation/app_routes.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: AppRoutes.newsList.path,
+    initialLocation: AppRoutes.mediaScreen.path,
     routes: <RouteBase>[
       HomeScreen.homeRoute(),
       MediaScreen.mediaRouter(),
